0ff6c5c61ad221b028a7deb3d4940d167e9c65ed
Tatiana <karamorina@gmail.com>
2020-10-18 01:40:41 +0300

    tests

--- src/components/TasksList/__tests__/TasksList.test.js
+++ src/components/TasksList/__tests__/TasksList.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {TasksList} from '../TasksList';
 import {mount, shallow} from 'enzyme';
-import {Task} from "../../Task/Task";
+import Task from "../../Task";
 import classes from "../TasksList.module.css";
+import {Message} from "../../../containers/Message/Message";
+import TaskAdd from "../../TaskAdd";
 
 const TASKS = [
     {id: 1, name: 'task1'},
@@ -11,22 +13,67 @@ const TASKS = [
 ]
 
 const PROJECT = {id: 1, name: 'TestProject', description: 'test description'};
+const PROJECT_INBOX = {id: null, name: 'Inbox', description: 'test description'};
+const PROJECT_FOCUS = {id: 'focus', name: 'Focus', description: 'test description'};
 
 describe('TasksList', () => {
-    it("Should call onLoad once after component rendering", () => {});
-    it("Should render nothing if project is still loading", () => {});
-    it("Should show special message if no project passed", () => {});
-    it("Should render name of the project for Inbox", () => {});
-    it("Should render name of the project for Focus", () => {});
-    it("Should render name of the project for normal project", () => {});
-    it("Should render description of the project, if passed", () => {});
-    it("Should not render description of the project, if empty", () => {});
+    it("Should call onLoad once after component rendering", () => {
+        const onLoad = jest.fn();
+        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT} onLoad={onLoad}/>);
+        expect(onLoad).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should render nothing if project is still loading", () => {
+        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT} isProjectsFetching={true}/>);
+        expect(wrapper.get(0)).toBeNull();
+    });
+
+    it("Should show special message if no project passed", () => {
+        const wrapper = shallow(<TasksList tasks={TASKS}/>);
+        expect(wrapper.find(Message)).toHaveLength(1);
+    });
+
+    it("Should render name of the project for Inbox", () => {
+        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT_INBOX}/>);
+        expect(wrapper.find('.' + classes.title).text()).toEqual('Inbox');
+    });
+
+    it("Should render name of the project for normal project", () => {
+                const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT}/>);
+        expect(wrapper.find('.' + classes.title).text()).toEqual(PROJECT.name);
+    });
+
+    it("Should render name of the project for Focus", () => {
+        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT_FOCUS}/>);
+        expect(wrapper.find('.' + classes.title).text()).toEqual('Focus');
+    });
+
+    it("Should render description of the project, if passed", () => {
+        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT}/>);
+        const description = wrapper.find('.' + classes.description);
+        expect(description).toHaveLength(1);
+        expect(description.text()).toEqual(PROJECT.description);
+    });
+
+    it("Should not render description of the project, if empty", () => {
+        const wrapper = shallow(<TasksList tasks={TASKS} project={{id: 1, name: 'projectName'}}/>);
+        const description = wrapper.find('.' + classes.description);
+        expect(description).toHaveLength(0);
+    });
 
     it("Should render same number of tasks as in props", () => {
-        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT} onLoad={() => {}}/>);
+        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT}/>);
         expect(wrapper.find('.' + classes.title)).toHaveLength(1);
         expect(wrapper.find(Task)).toHaveLength(3);
     });
-    it("Should hide Add Task button if project is Focus", () => {});
-    it("Should show Add Task button if project is not Focus", () => {});
+
+    it("Should hide Add Task button if project is Focus", () => {
+        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT_FOCUS}/>);
+        expect(wrapper.find(TaskAdd)).toHaveLength(0);
+    });
+
+    it("Should show Add Task button if project is not Focus", () => {
+        const wrapper = shallow(<TasksList tasks={TASKS} project={PROJECT}/>);
+        expect(wrapper.find(TaskAdd)).toHaveLength(1);
+    });
 })
